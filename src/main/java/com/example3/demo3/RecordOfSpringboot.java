d9fb6976974c963bb9895df26a47056eb3fc6d46
baotingyu <1924789952@qq.com>
2021-08-13 17:02:45 +0800

    First commit3

---
+++ src/main/java/com/example3/demo3/RecordOfSpringboot.java
@@ -0,0 +1,14 @@
+package com.example3.demo3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RecordOfSpringboot {
+
+    Map<String , Object> map = new HashMap<String ,Object>();
+            map.put("user", frameworkService.getPersonInfo(userGetId()));
+            map.put("System", frameworkService);
+            map.put("menus", frameworkService);
+            map.put("roles", frameworkService);
+
+}
